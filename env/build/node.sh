eaeda64d2cdf154d6cb405d8122b380d12bd2020
Victor Gras <victorgras@gmail.com>
2014-06-28 22:45:29 +0200

    added nmp installation and cleared Vagrantfile

--- env/build/node.sh
+++ env/build/node.sh
@@ -7,6 +7,7 @@ echo ""
 
 #enable add-apt-repository command
 
+apt-get update
 apt-get install -y python-software-properties
 
 #install nodejs from chris lea repo
@@ -15,3 +16,9 @@ add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
 apt-get install -y python-software-properties python g++ make nodejs
 
+#install nmp and add npm to path
+
+apt-get install -y npm
+npm config set prefix ~/npm
+PATH=$HOME/npm/bin:$PATH
+echo 'export PATH=$HOME/npm/bin:$PATH' >>~/.bashrc
